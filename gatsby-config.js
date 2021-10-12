0b10512e15e7acbb7a1641e80e2a8a62bd3a3625
Christophe Moreira <moreira.christophe@outlook.com>
2021-10-12 11:16:26 +0100

    Finished Gatsby Tutorial Part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,16 @@ module.exports = {
     siteUrl: 'https://www.yourdomain.tld',
     title: 'My Gatsby Site',
   },
-  plugins: ['gatsby-plugin-image', 'gatsby-plugin-sharp'],
+  plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    'gatsby-plugin-mdx',
+  ],
 };
